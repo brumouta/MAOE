51a3efed85f5729a01c750d9f66a735696502a28
Ricardo Godoy <2732629@gmail.com>
2013-01-30 20:54:52 -0200

    adcionado arquivo de javascript para webapp

---
+++ javascripts/webapp.js
@@ -0,0 +1,26 @@
+var pickImage = document.querySelector("#pick-image");
+if (pickImage) { 
+    pickImage.onclick = function () {
+     var pick = new MozActivity({
+         name: "pick",
+         data: {
+             type: ["image/png", "image/jpg", "image/jpeg"]
+         
+            }
+    });
+
+        pick.onsuccess = function () {
+
+            var img = document.createElement("img");
+            img.src = window.URL.createObjectURL(this.result.blob);
+            var imagePresenter = document.querySelector("#image-presenter");
+            imagePresenter.appendChild(img);
+            imagePresenter.style.display = "block";
+        };
+
+        pick.onerror = function () {
+
+            alert("Can't view the image!");
+        };
+    }
+}
